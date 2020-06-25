b46f8a6b755f322b85f65226b5b13bc6ed53ec42
sutin <sutin.i@quo-global.com>
2020-06-26 03:11:52 +0700

    added admin-lte3 theme

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-admin-lte';
+  theme_path = 'admin-lte'
+}
